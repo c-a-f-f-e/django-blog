5d93ee3ea360ab04036433148a4a5c274968e7ef
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-10-13 21:09:17 +0200

    新規投稿用のURLの作成

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,11 @@ from app import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'), # 追加
+    path('post/new/', views.CreatePostView.as_view(), name='post_new'), # 追加
 ]
+
+class XXXXX(View):
+    def get(self, request, *args, **kwargs):
+        # 表示したら必ず最初に呼ばれる
+    def post(self, request, *args, **kwargs):
+        # 画面で送信ボタンなど、submitされた時に呼ばれる
