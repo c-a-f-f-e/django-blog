44732d7bbe23f64b56dbfb52b6bf58380ddec84d
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-10-13 20:11:46 +0200

    詳細画面のview作成

--- app/views.py
+++ app/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 from .models import Post
 
 
-class IndexView(View):
+class PostDetailView(View):
     def get(self, request, *args, **kwargs):
-        post_data = Post.objects.order_by("-id")
-        return render(request, 'app/index.html', {
-            'post_data': post_data,
+        post_data = Post.objects.get(id=self.kwargs['pk'])
+        return render(request, 'app/post_detail.html', {
+            'post_data': post_data
         })
 
 # Create your views here.
