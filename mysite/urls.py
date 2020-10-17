4106620519419ff9b94c4d29819393a6afdf82c3
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-10-17 18:43:37 +0200

    indicate the url

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static # 追加
+from django.conf import settings # 追加
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
