7d9378be88994c0fc6519e75e4bd31045f12684f
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-10-18 18:48:07 +0200

    modificate the view

--- app/views.py
+++ app/views.py
@@ -21,7 +21,16 @@ class PostDetailView(View):
 
 class CreatePostView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
-        form = PostForm(request.POST or None)
+        post_data = Post.objects.get(id=self.kwargs['pk'])
+        form = PostForm(
+            request.POST or None,
+            initial={
+                'title': post_data.title,
+                'category': post_data.category,
+                'content': post_data.content,
+                'image': post_data.image,
+            }
+        )
 
         return render(request, 'app/post_form.html', {
             'form': form
@@ -34,6 +43,9 @@ class CreatePostView(LoginRequiredMixin, View):
             post_data = Post()
             post_data.author = request.user
             post_data.title = form.cleaned_data['title']
+            category = form.cleaned_data['category']
+            category_data = Category.objects.get(name=category)
+            post_data.category = category_data
             post_data.content = form.cleaned_data['content']
             if request.FILES:
                 post_data.image = request.FILES.get('image') # 追加
@@ -88,4 +100,12 @@ class PostDeleteView(LoginRequiredMixin, View):
         post_data = Post.objects.get(id=self.kwargs['pk'])
         post_data.delete()
         return redirect('index')
+
+
+class CategoryView(View):
+    def get(self, request, *args, **kwargs):
+        category_data = Category.objects.get(name=self.kwargs['category'])
+        post_data = Post.objects.order_by('-id').filter(category=category_data)
+        return render(request, 'app/index.html', {
+            'po
 # Create your views here.
