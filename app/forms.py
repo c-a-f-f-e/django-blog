b3be7cd454ab563edd55d2d1db691e1751d59895
c-a-f-f-e <yoshi-da0406@outlook.jp>
2020-10-18 17:58:13 +0200

    make the form for able to select the category

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from .models import Category
 
 
+class PostForm(forms.Form):
+    category_data = Category.objects.all()
+    category_choice = {}
+    for category in category_data:
+        category_choice[category] = category
+        
 class PostForm(forms.Form):
     title = forms.CharField(max_length=30, label='タイトル')
     content = forms.CharField(label='内容', widget=forms.Textarea())
